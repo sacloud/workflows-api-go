419262e6ddbfab4ea98280d4287e6885802632f3
a2not <31874975+a2not@users.noreply.github.com>
2025-12-02 14:00:10 +0900

    chore: add license header to workflow_test.go
    Added the Apache 2.0 license header to the top of workflow_test.go for
    compliance. Also fixed a variable shadowing issue in the deferred cleanup
    function by declaring a new 'err' variable.
    
    Signed-off-by: a2not <31874975+a2not@users.noreply.github.com>

--- workflow_test.go
+++ workflow_test.go
@@ -1,3 +1,17 @@
+// Copyright 2025- The sacloud/workflows-api-go Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package workflows_test
 
 import (
@@ -36,7 +50,7 @@ func TestWorkflowAPI(t *testing.T) {
 
 	defer func() {
 		// DeleteWorkflow
-		err = workflowAPI.Delete(ctx, respCreate.ID)
+		err := workflowAPI.Delete(ctx, respCreate.ID)
 		require.NoError(t, err)
 	}()
 
